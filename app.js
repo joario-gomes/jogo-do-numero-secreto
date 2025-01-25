bdba630f93b8fc4a81ccf99b13a73f6d8ad4f8a3
joario-gomes <joario.gomes@outlook.com>
2025-01-24 21:25:27 -0300

    Update app.js

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ function exibirTextoNaTela(tag, texto){
     if ('speechSynthesis' in window) {
         let utterance = new SpeechSynthesisUtterance(texto);
         utterance.lang = 'pt-BR'; 
-        utterance.rate = 1.2; 
+        utterance.rate = 1.8; 
         window.speechSynthesis.speak(utterance); 
     } else {
         console.log("Web Speech API não suportada neste navegador.");
